933f8050edb26fc01630f08008a344ef7c77e038
RayKilburg <rkilburg42@gmail.com>
2018-08-05 22:08:20 -0400

    workn

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES('Big Burger');
+INSERT INTO burgers (burger_name) VALUES('Little Burger');
+INSERT INTO burgers (burger_name) VALUES('Western Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES('Big Kahuna Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES('Purple Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES('Rest In Peas Burger', true);
--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// serve static content for the app from the "public" directory
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extened: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("View enine", "handlebars");
+
+// import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// start our server so it can begin listening to client requests
+app.listen(PORT, function() {
+  // log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
