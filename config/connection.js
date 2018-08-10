36903b63b989f3cd5d5d0c2d315ffa79a59f0344
Ray <rkilburg42@gmail.com>
2018-08-10 16:09:42 -0400

    Update connection

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,27 @@
 // set up mysql connection
 var mysql = require("mysql");
 
-var connection;
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root",
-//   database: "burger_db"
-// });
+// make  local connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "burger_db"
+});
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  // make  local connection
-   connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
   });
-  // connection.connect(function(err) {
-  //   if (err) {
-  //     console.error("error connecting: " + err.stack);
-  //     return;
-  //   }
-  //   console.log("connected as id " + connection.threadId);
-  // });
-}
+//}
 
 // export connection for ot ORM to use
 module.exports = connection;
