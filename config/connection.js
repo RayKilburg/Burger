40c38816907ac88f6ffe0ed1514109e8de333db2
Ray <rkilburg42@gmail.com>
2018-08-10 15:38:28 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -9,14 +9,18 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
-// make connection
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // make  local connection
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+}
 
 // export connection for ot ORM to use
 module.exports = connection;
